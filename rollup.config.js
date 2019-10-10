32328ec84638839a7de15dd013242840909bc944
Anh <anhoev@yahoo.de>
2019-10-10 16:42:41 +0700

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import minify from 'rollup-plugin-babel-minify'
+
+export default {
+  input: 'src/index.js',
+  plugins: [minify({ comments: false })],
+  output: [
+    {
+      file: 'dist/plugin.cjs.js',
+      format: 'cjs',
+    },
+  ],
+}
